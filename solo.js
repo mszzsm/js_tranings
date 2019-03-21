bef8a067df8b8aab886da7f5a54b53f52ad54721
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-03-21 19:20:23 +0100

    .commit

--- solo.js
+++ solo.js
@@ -1,6 +1,9 @@
-var arr1, arr2, arr;
+newFunction();
+function newFunction() {
+    var arr1, arr2, arr;
+    arr1 = new Array(1, 0, 2);
+    arr2 = new Array(4, 2, 0);
+    arr = arr2.concat(arr1);
+    console.log(arr[arr[2]]);
+}
 
-arr1 = new Array(1,0,2);
-arr2 = new Array(4,2,0);
-arr = arr2.concat(arr1);
-console.log(arr[arr[2]]);
