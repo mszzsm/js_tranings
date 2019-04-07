6e1f7b85a5c845bda074971f072be0d7533cb8d3
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-04-07 14:55:02 +0200

    commit

--- sorting.js
+++ sorting.js
@@ -1,26 +1,49 @@
-function quickSort(arr, start, end) {
+let values = [];
+let w = 20;
+
+function setup(){
+    createCanvas(800, 300);
+    values = new Array(floor(width / w));
+    for (let i = 0; i < values.length; i++){
+        values[i] = random(height);
+    }
+    quickSort(values, 0, values.length - 1);
+}
+
+async function quickSort(arr, start, end) {
     if (start >= end) {
         return;
     }
-    let index = partition(arr, start, end);
+    let index = await partition(arr, start, end);
 
-    quickSort(arr, start, index - 1);
-    quickSort(arr, index + 1, end)
+    await Promise.all([
+    await quickSort(arr, start, index - 1),
+    await quickSort(arr, index + 1, end)
+ ]);
 }
 
 function partition(arr, start, end) {
-    let pivotIndex = 0;
+    let pivotIndex = start;
     let pivotValue = arr[end];
-    for (let i = 0; i < end; i++) {
+    for (let i = start; i < end; i++) {
         if (arr[i] < pivotValue){
-            swap(i, pivotIndex);
+            swap(arr, i, pivotIndex);
             pivotIndex++;
         }
     }
+    swap(arr, pivotIndex, end);
+    return pivotIndex;
 }
 
-function swap(arr, a, b){
+async function swap(arr, a, b){
+
+    await sleep(1000)
+        
     let temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
